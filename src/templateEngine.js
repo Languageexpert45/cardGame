bb9f0f6fd150d025c72fdfd7c789f89ca28ef13c
Alex <cocos_@mail.ru>
2022-10-12 13:54:58 +0300

    HW3

--- src/templateEngine.js
+++ src/templateEngine.js
@@ -1,25 +1,30 @@
+/* eslint-disable prettier/prettier */
+/* eslint-disable no-unused-vars */
 function templateEngine(block) {
     if (block === undefined || block === null || block === false) {
-        return document.createTextNode('');
+        return document.createTextNode("");
     }
-    if (typeof block === 'string' || typeof block === 'number' || block === true) {
+    if (
+        typeof block === "string" ||
+        typeof block === "number" ||
+        block === true
+    ) {
         return document.createTextNode(block);
     }
     if (Array.isArray(block)) {
         const fragment = document.createDocumentFragment();
 
-        block.forEach(element => {
+        block.forEach((element) => {
             fragment.appendChild(templateEngine(element));
         });
 
         return fragment;
     }
-
     const result = document.createElement(block.tag);
 
     if (block.cls) {
         const classes = [].concat(block.cls);
-        classes.forEach(cls => {
+        classes.forEach((cls) => {
             result.classList.add(cls);
         });
     }
@@ -27,7 +32,7 @@ function templateEngine(block) {
     if (block.attrs) {
         const keys = Object.keys(block.attrs);
 
-        keys.forEach(key => {
+        keys.forEach((key) => {
             result.setAttribute(key, block.attrs[key]);
         });
     }
